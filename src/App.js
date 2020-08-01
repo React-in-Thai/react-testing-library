4497bdb6df724f04bf77afdb855c1a36c092c291
siriwatknp <siriwatkunaporn@gmail.com>
2020-08-01 22:08:44 +0700

    feat: 🎸 live

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import './App.css';
-import './tailwind.output.css';
+import React, { useState } from "react";
+import { getHeroDetail } from "./api";
+import "./tailwind.output.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const handleSubmit = async () => {
+    setLoading(true);
+    const response = await getHeroDetail(text);
+    setData(response);
+    setLoading(false);
+  };
   return (
-    <div>
+    <div className="max-w-xl mx-auto py-5">
+      <label
+        className="block mb-2 text-gray-700 text-sm font-bold"
+        htmlFor="hero-name"
+      >
+        Search
+      </label>
+      <div className="flex gap-2">
+        <input
+          className="shadow appearance-none border rounded py-2 px-4 text-gray-700"
+          id="hero-name"
+          placeholder="Type a hero name"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          className="inline-flex rounded bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+      {loading && <div className="py-5">loading</div>}
+      {data && (
+        <div className="flex gap-4 mt-5">
+          <img className="w-56 h-56 rounded" alt={`Avatar of ${data.name}`} src={data.avatar} />
+          <div className="flex-auto">
+            <div className="font-bold text-2xl mb-2">{data.name}</div>
+            <div className="text-gray-700">{data.description}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,72 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
+import { getHeroDetail } from "./api";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('./api')
+
+const SUPERMAN = {
+  id: 1,
+  name: 'Superman',
+  avatar: 'https://cdn.theatlantic.com/thumbor/xuePShEYRyEQec_THgWcYFhYLnw=/540x0:2340x1800/500x500/media/img/mt/2016/01/superman/original.jpg',
+  description: 'Superman is a fictional superhero. The character was created by writer Jerry Siegel and artist Joe Shuster, and first appeared in the comic book Action Comics #1 (cover-dated June 1938 and published April 18, 1938).[1] The character regularly appears in comic books published by DC Comics, and has been adapted to a number of radio serials, movies, and television shows.',
+}
+getHeroDetail.mockResolvedValue(SUPERMAN)
+// getHeroDetail.mockRejectedValue()
+
+const renderApp = () => {
+  render(<App />)
+  const getInput = () => screen.getByLabelText(/search/i)
+  const getSubmit = () => screen.getByRole('button', { name: /submit/i })
+  return {
+    getInput,
+    getSubmit,
+    runSearchJourney: async (name, pendingCallback) => {
+      userEvent.type(getInput(), name) // input
+      userEvent.click(getSubmit())
+      if (pendingCallback) pendingCallback()
+      await waitForElementToBeRemoved(() => screen.getByText('loading'))
+    }
+  }
+}
+
+describe('App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should display input & submit button', () => {
+    const { getInput, getSubmit } = renderApp()
+    // test input existence
+    getInput()
+    getSubmit()
+  })
+
+  it('should call "getHeroDetail" with name when submitted', async () => {
+    const { runSearchJourney } = renderApp()
+
+    await runSearchJourney('superman')
+
+    expect(getHeroDetail).toHaveBeenCalledWith('superman')
+  })
+
+  it('should render loading while calling api', async () => {
+    const { runSearchJourney } = renderApp()
+
+    await runSearchJourney('superman', () => {
+      screen.getByText('loading')
+    })
+
+    expect(getHeroDetail).toHaveBeenCalledWith('superman')
+  })
+
+  it('should render avatar, name & description from response', async () => {
+    const { runSearchJourney } = renderApp()
+
+    await runSearchJourney('superman')
+
+    screen.getByText(SUPERMAN.name)
+    screen.getByText(SUPERMAN.description)
+    screen.getByAltText(`Avatar of ${SUPERMAN.name}`)
+  })
+})
